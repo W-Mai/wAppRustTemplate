cb991ea98ad82d45f3a106082709ce2de3fefbc4
W-Mai <1341398182@qq.com>
2022-10-08 01:44:04 +0800

    add `README` and update `build.sh`
    Signed-off-by: W-Mai <1341398182@qq.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+# App Rust Template
+
+To build this project:
+- change `cp_target_path` in `build.sh` file first.
+```shell
+./build.sh
+```
+
+### Note
+
+Nightly Rust needed:
+```shell
+rustup override set nightly 
+```
+
+And require `wabt` `binaryen` tools
+
+#### For macOS
+```shell
+brew install wabt binaryen
+```
--- build.sh
+++ build.sh
@@ -1,18 +1,18 @@
-source ~/.zshrc
+#!/usr/bin/env bash
 
 target_path=target/wasm32-wasi/release
 target_name=app.wasm
 target=${target_path}/$target_name
-cp_target_path=/Users/w-mai/Documents/Projects/X-TRACK-EVO/Firmware/X-TRACK-EVO/Vendor/Simulator/cmake-build-debug/bin
+
+# Simulator Compiled binary path
+cp_target_path="path/to/bin"
 
 cargo build --release --target wasm32-wasi
 wasm-opt $target -Os --vacuum -o ${target}.opt
 cp ${target}.opt ${target}.opt.old
 wasm-strip ${target}.opt
 
-l $target_path | grep ".*wasm"
-
-#wasm3 --func fuck ${target}.opt
+ls -alh ${target_path}/*.wasm*
 
 cp ${target}.opt ${cp_target_path}/test.wasm
 
