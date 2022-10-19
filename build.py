6c87615255a6884f9a85e51b2e33c920289c87ea
W-Mai <1341398182@qq.com>
2022-10-19 13:51:55 +0800

    update
    Signed-off-by: W-Mai <1341398182@qq.com>

--- build.py
+++ build.py
@@ -17,7 +17,7 @@ sim_path = os.environ.get("SIM_PATH", "../../../Vendor/Simulator/build")
 
 
 def build_and_optimize():
-    log("⌚", "Start Build")
+    log("⌚️", "Start Build")
     if run(["cargo", "build", "--release", "--target", "wasm32-wasi"]).returncode:
         log("😭😭😭", "Build Failed")
         exit()
